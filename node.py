d7caa2b3421ff85b9187461b45ae148e9e2d6f94
Jakub <kuba.staniszewski.ks@gmail.com>
2024-04-27 22:12:35 +0200

    add more properties to node.py

--- node.py
+++ node.py
@@ -1,6 +1,9 @@
 class Node:
-    def __init__(self, turn, whoseMove, gameState, whoWon):
+    def __init__(self, turn, whoseMove, gameState, whoWon, parent, children, depth):
         self.turn = turn
         self.whoseMove = whoseMove
         self.gameState=gameState
         self.whoWon=whoWon
+        self.parent=parent
+        self.children=children
+        self.depth=depth
