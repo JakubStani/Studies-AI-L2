b822162e90571c80406a32b8a49652cfcb09c47a
Jakub <kuba.staniszewski.ks@gmail.com>
2024-05-09 21:27:06 +0200

    fix building all possible moves tree and add print methods

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import copy
 from counter import Counter
 from node import Node 
 
+maxDepth=3
+
 def prepareStartGameState():
     gameState={
         'gameboardState': [],
@@ -13,7 +15,7 @@ def prepareStartGameState():
         for x in range(16):
             if(x<counters):
                 gameState['gameboardState'][y].append('1')
-                gameState['counters'][f'{x}-{y}-1']= Counter(x, y, '1')
+                gameState['counters']['1'][f'{x}-{y}-1']= Counter(x, y, '1')
 
             else:
                 gameState['gameboardState'][y].append('0')
@@ -32,7 +34,7 @@ def prepareStartGameState():
         for x in list(range(16))[::-1]:
             if(11<=y and x>15-counters):
                 arrayToPrepare.append('2')
-                gameState['counters'][f'{x}-{y}-2']= Counter(x, y, '2')
+                gameState['counters']['2'][f'{x}-{y}-2']= Counter(x, y, '2')
             else:
                 arrayToPrepare.append('0')
         if(y>=11 and y<14):
@@ -59,26 +61,66 @@ def printGameboardState(gameboardState):
     print('__________________________________________________')
     print("   ||  0    1    2    3    4    5    6    7    8    9    10   11   12   13   14   15")
 
+def printAllChildrenStates(parentNode):
+    if(not parentNode._children()==None):
+        for i in range(len(parentNode._children())):
+            print(f'dziecko: {i}')
+            printGameboardState(parentNode._children()[i]._gameState()['gameboardState'])
+            print()
+
+#UWAGA!: trwa baaardzo długo, ponieważ drzewo jest gromne
+def printWholeTreeStates(parentNode):
+    if(not parentNode._children()==None):
+        print(f'______________RUNDA: {parentNode._round()}_________')
+        for i in range(len(parentNode._children())):
+            if(parentNode._round()==1):
+                print(f'dziecko: {i}')
+                printGameboardState(parentNode._children()[i]._gameState()['gameboardState'])
+                print()
+            printWholeTreeStates(parentNode._children()[i])
+
+def printAllTreeLeafs(parentNode):
+    if(not parentNode._children()==None):
+        for i in range(len(parentNode._children())):
+            printAllTreeLeafs(parentNode._children()[i])
+    else:
+        print(f'______________RUNDA: {parentNode._round()}_________')
+        printGameboardState(parentNode._gameState()['gameboardState'])
+
+def printOneLeaf(parentNode):
+    if(not parentNode._children()==None):
+        printOneLeaf(parentNode._children()[0])
+    else:
+        print(f'______________RUNDA: {parentNode._round()}_________')
+        printGameboardState(parentNode._gameState()['gameboardState'])
+
+
+
+
+
 #na wejściu jest parent, który ma już dzieci
+#podana runda, to runda, która zostanie przypisana dzieciom parentNoda
 def generatePossibleMovesTree(whoseMove, gameState, parentNode, round):
-    if not parentNode._children() == None and round <3: #TODO: ustaw jakąś zmienną na maksymalną głębokość
+    global maxDepth
+    if not parentNode._children() == None and round <maxDepth: #TODO: ustaw jakąś zmienną na maksymalną głębokość
         for child in parentNode._children():
-            generateAllPossibleMovesInThisRound(whoseMove, child._gameState(), child, round)
+            generateAllPossibleMovesInThisRound(whoseMove, child._gameState(), child, round+1)
             nextWhoseMove=str((int(whoseMove)%2)+1)
             generatePossibleMovesTree(nextWhoseMove, child._gameState(), child, round+1)
     else: #TODO: sprawdź i ustaw, kto wygrał
         pass
 
+#TODO: pozbądź siękluczy współrzędnych pionków (bo one będą sięzmieniać) i zmieniaj współrzędne pionka po jego ruchu
 #na wejściu jest parent, który nie ma dzieci
 def generateAllPossibleMovesInThisRound(whoseMove, gameState, parentNode, round):
     allPossibleMoves=[]
     for counter in list(gameState['counters'][whoseMove]):
         #sprawdzamy możliwości jego ruchu -> allCounterMoves to lista węzłów
-        allCounterMoves = allPossibleMoveForCounter(counter._x, counter._y, whoseMove, gameState, parentNode, round)
+        allCounterMoves = allPossibleMoveForCounter(gameState['counters'][whoseMove][counter]._x(), gameState['counters'][whoseMove][counter]._y(), whoseMove, gameState, False, parentNode, round, gameState['counters'][whoseMove][counter]._x(), gameState['counters'][whoseMove][counter]._y())
         allPossibleMoves.extend(allCounterMoves)
     parentNode.setChildren(allPossibleMoves)
 
-def allPossibleMoveForCounter(x, y, whoseMove, gameState, jumpOnly, parentNode, round):
+def allPossibleMoveForCounter(x, y, whoseMove, gameState, jumpOnly, parentNode, round, previousX, previousY):
     possibleStates=[]
 
     #sprawdzamy wszystkie możliwe ruchy i zapisujemy możliwe stany
@@ -86,51 +128,58 @@ def allPossibleMoveForCounter(x, y, whoseMove, gameState, jumpOnly, parentNode, 
 
         if(whoseMove=='1'):
             #ruch w dół o 1
-            possibleStates=moveInDirection(y+1,x,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y+1,x,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
 
             #ruch do prawego dolnego rogu
-            possibleStates=moveInDirection(y+1,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y+1,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
             
             #ruch do prawej
-            possibleStates=moveInDirection(y,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
 
         else:
             #ruch do góry o 1
-            possibleStates=moveInDirection(y-1,x,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y-1,x,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
             
             #ruch do lewej
-            possibleStates=moveInDirection(y,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
             
             #ruch do lewego górnego rogu
-            possibleStates=moveInDirection(y-1,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+            possibleStates=moveInDirection(y-1,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
         
         #ruch do prawego górnego rogu
-        possibleStates=moveInDirection(y-1,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+        possibleStates=moveInDirection(y-1,x+1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
 
         #ruch do lewego dolnego rogu
-        possibleStates=moveInDirection(y+1,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round)
+        possibleStates=moveInDirection(y+1,x-1,y,x,whoseMove,gameState,possibleStates, jumpOnly, parentNode, round, previousX, previousY)
 
         return possibleStates
 
         #jak daleki skok
         # for up in range(2,15,2):
-
+#TODO: napraw nieustanne skakanie po przekątnej
 #pozwala na rekurencyjne sprawdzanie możliwych ruchów
-def moveInDirection(yto,xto,ycurrent,xcurrent,whoseMove,gameState, possibleStates, jumpOnly, parentNode, round):
-    #ruch w dół o 1
-    if 0<=yto and yto<=15 and 0<=xto and xto<=15:
-        #sprawdzamy, w jakim kierunku jest ruch
-        displacement=[xto-xcurrent, yto-ycurrent]
-        if gameState['gameboardState'][yto][xto] == '0' and not jumpOnly:
-            possibleStates.append(moveClose(yto, xto, ycurrent, xcurrent, whoseMove, gameState, parentNode, round))
-        else:
-            if not gameState['gameboardState'][yto][xto] == '0':
-                #sprawdzamy, czy skok będzie jeszcze w granicy planszy
-                if 0<=yto + displacement[1] and yto + displacement[1]<= 15 and 0<=xto + displacement[0] and xto + displacement[0]<=15:
-                    if gameState['gameboardState'][yto + displacement[1]][xto + displacement[0]] == '0':
-                        possibleStates.append(moveClose(yto + displacement[1], xto + displacement[0], ycurrent, xcurrent, whoseMove, gameState))
-                        newStates=allPossibleMoveForCounter(xto + displacement[0], yto + displacement[1], whoseMove, gameState, True, parentNode, round)
-                        possibleStates.extend(newStates)
+def moveInDirection(yto,xto,ycurrent,xcurrent,whoseMove,gameState, possibleStates, jumpOnly, parentNode, round, previousX, previousY):
+
+    #warunek chroniący przed zapętleniem-> wykonaniem ruchu w miejsce, w którym było się przed chwilą
+    if(not yto==previousY or not xto == previousX):
+
+        #ruch w dół o 1
+        if 0<=yto and yto<=15 and 0<=xto and xto<=15:
+            #sprawdzamy, w jakim kierunku jest ruch
+            displacement=[xto-xcurrent, yto-ycurrent]
+            if gameState['gameboardState'][yto][xto] == '0' and not jumpOnly:
+                possibleStates.append(moveClose(yto, xto, ycurrent, xcurrent, whoseMove, gameState, parentNode, round))
+            else:
+                if not gameState['gameboardState'][yto][xto] == '0':
+                    #sprawdzamy, czy skok będzie jeszcze w granicy planszy
+                    if 0<=yto + displacement[1] and yto + displacement[1]<= 15 and 0<=xto + displacement[0] and xto + displacement[0]<=15:
+                        if gameState['gameboardState'][yto + displacement[1]][xto + displacement[0]] == '0': #TODO: napraw skakanie w to samo miejsce (bo po przekątnej)
+                            nState=moveClose(yto + displacement[1], xto + displacement[0], ycurrent, xcurrent, whoseMove, gameState, parentNode, round)
+                            possibleStates.append(nState)
+                            previousX = xto
+                            previousY = yto
+                            newStates=allPossibleMoveForCounter(xto + displacement[0], yto + displacement[1], whoseMove, nState._gameState(), True, parentNode, round, previousX, previousY)
+                            possibleStates.extend(newStates)
     return possibleStates
 
 #wykonuje ruch w określonym kierunku i zwraca stan po ruchu
@@ -147,8 +196,10 @@ def moveClose(yto, xto, ycurrent, xcurrent, whoseMove, gameState, parentNode, ro
 
     #przekształcamy wynik na węzeł
 
-    return Node(round, whoseMove, possibSt, '0', parentNode, None, round) #TODO: zmień kto wygrał na właściwą wartość
+    return Node(round, whoseMove, possibSt, '0', parentNode, None, round, testDoNothing, testDoNothing) #TODO: zmień kto wygrał i dwie funkcje liczące heurystyki na właściwą wartość
         
+def testDoNothing(arg1, arg2):
+    pass
 #TODO: sprawdzaj wszystkie możliwe skoki (przeskakiwanie przez następne pionki)
 
 # #sprawdza w pętli, jak daleko może skoczyć
@@ -257,8 +308,15 @@ if __name__=='__main__':
     #     printGameboardState(pos)
     # game(gameState)
 
-    parentNode = Node(0, None, gameState, '0', None, None, 0)
+    parentNode = Node(0, None, gameState, '0', None, None, 0, testDoNothing, testDoNothing)
     generateAllPossibleMovesInThisRound('1', gameState, parentNode, 1)
 
-    generatePossibleMovesTree('1', gameState, parentNode, 1)
+    #tutaj już zmiana gracza, bo w powyższych dwóch linijkach wygenerowaliśmy ruchy dla gracza 1
+    generatePossibleMovesTree('2', gameState, parentNode, 1)
+
+    printOneLeaf(parentNode)
+
+    print('Koniec programu')
+
+    # allPossibleMoveForCounter(0, 0, '1', gameState, False, parentNode, 1, 0, 0)
 
